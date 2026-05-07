5564b2b4fdf8a15fb892a8feda494f847c55a3fc
JoshuaHong0215 <JoshuaHong0215>
2026-05-07 17:01:05 +0900

    first commit

---
+++ test_02.py
@@ -0,0 +1,36 @@
+#launch Isaac Sim before any other imports
+#default first two lines in any standalone application
+from isaacsim import SimulationApp
+simulation_app = SimulationApp({"headless": False}) # we can also run as headless.
+
+from isaacsim.core.api import World
+from isaacsim.core.api.objects import DynamicCuboid
+import numpy as np
+
+world = World()
+world.scene.add_default_ground_plane()
+fancy_cube =  world.scene.add(
+    DynamicCuboid(
+        prim_path="/World/random_cube",
+        name="fancy_cube",
+        position=np.array([0, 0, 5.0]),
+        scale=np.array([2.0, 2.0, 2.0]),
+        color=np.array([0, 0, 1.0]),
+    ))
+# Resetting the world needs to be called before querying anything related to an articulation specifically.
+# Its recommended to always do a reset after adding your assets, for physics handles to be propagated properly
+world.reset()
+
+# app을 계속 실행
+while simulation_app.is_running():
+    position, orientation = fancy_cube.get_world_pose()
+    linear_velocity = fancy_cube.get_linear_velocity()
+    # will be shown on terminal
+    print("Cube position is : " + str(position))
+    print("Cube's orientation is : " + str(orientation))
+    print("Cube's linear velocity is : " + str(linear_velocity))
+    # we have control over stepping physics and rendering in this workflow
+    # things run in sync
+    world.step(render=True) # execute one physics step and one rendering step
+
+simulation_app.close() # close Isaac Sim
